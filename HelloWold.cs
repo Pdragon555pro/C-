ca053b512ddd51e53292b562460bb5b4cc79f68d
Pdragon555l <89274125+Pdragon555pro@users.noreply.github.com>
2023-11-05 12:00:25 +0300

    Add files via upload

---
+++ HelloWold.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWold : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //yes yes yes
+        print("Hello Wrold");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
